d00b5ca18a2d57759ef80c628a1b6f80d71b1db6
Arnaudboy <r.boyer895@laposte.net>
2021-06-16 16:45:10 +0200

    reconfiguration de venv

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ app.layout = html.Div([html.Div(dcc.Graph(id='map', clickData={'points': [{'hove
             dcc.Slider(id="year_selector",
             min=df['year'].min(), max=df['year'].max(), value=df['year'].max(), step=None,
             marks={str(year): str(year) for year in df['year'].unique()}),
-            html.Div([dcc.Dropdown(id="dropdown1", options=[{'label': i, 'value': i} for i in ['lifeExp', 'pop', 'gdpPercap']]), 
+            html.Div([dcc.Dropdown(id="dropdown1", options=[{'label': i, 'value': i} for i in ['lifeExp', 'pop', 'gdpPercap']], value="pop"), 
             dcc.Graph(id='graph1'), 
-            dcc.Dropdown(id="dropdown2", options=[{'label': i, 'value': i} for i in ['lifeExp', 'pop', 'gdpPercap']]), 
+            dcc.Dropdown(id="dropdown2", options=[{'label': i, 'value': i} for i in ['lifeExp', 'pop', 'gdpPercap']], value='lifeExp'), 
             dcc.Graph(id='graph2')])
             ])
 
@@ -29,14 +29,14 @@ def render_map(year) :
     return fig
 
 @app.callback(Output('graph1', 'figure'), [Input('map', 'clickData'), Input('dropdown1', 'value')])
-def display_graph1(clickValue, type):
+def display_graph1(clickValue, type='pop'):
     dff = df[df['country']==clickValue['points'][0]['hovertext']]
     fig = px.scatter(x=dff['year'], y=dff[str(type)])
     fig.update_traces(mode='lines+markers')
     return fig
 
 @app.callback(Output('graph2', 'figure'), [Input('map', 'clickData'), Input('dropdown2', 'value')])
-def display_graph2(clickValue, type):
+def display_graph2(clickValue, type='lifeExp'):
     dff = df[df['country']==clickValue['points'][0]['hovertext']]
     fig = px.scatter(x=dff['year'], y=dff[str(type)])
     return fig
