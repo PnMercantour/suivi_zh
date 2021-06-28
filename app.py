6b481807d94cd3e5863dd0f9a55eacb7e779f5b4
Arnaudboy <r.boyer895@laposte.net>
2021-06-28 19:41:35 +0200

    la carte se met à jour en fonction du site selectionné dans la liste. Pas de possibilité de sélectionné un site depuis la carte ou de revenir à aucune sélection

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import plotly.express as px
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State, MATCH, ALL
 import pandas as pd
 import json
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -15,26 +15,33 @@ points = pd.read_csv("data/sites.csv", ';')
 zh = pd.read_csv("data/zh.csv", ';')
 #création d'un dictionaire pour les couleurs des polygones
 color = {'bon': 'green', 'moyen': 'yellow', 'mauvais': 'red'}
-# Ces lignes ne sont utiles que pour inverser les entrées des tableaux des csv. Sinon le parc se retrouve en Ethiopie
-coor = [{'lat' : json.loads(points['centroid'][i])['coordinates'][1], 'lon' : json.loads(points['centroid'][i])['coordinates'][0]} for i in range(len(points))]
+
+# GeoJSON pour les points
+sites = dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry" : json.loads(point)} for point in points['centroid']]})
+# GeoJSON pour les zones humides
+zones_humides = dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry":json.loads(site)}for site in zh['geojson']]})
 
 # Création de la dataframe, un tableau passé en paramètre de dl.Map dans le layout
 # Le fond de carte
 df = [dl.WMSTileLayer(url="http://ows.mundialis.de/services/service?",
-                    layers="TOPO-OSM-WMS", format="image/png")]
-# Ajout des points
-for obj in coor:
-    df.append(dl.GeoJSON(data=dlx.dicts_to_geojson([obj])))
-# Ajout des polygones
-for item in zh['geojson']:
-    df.append(dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry":json.loads(item)}]}))
-
+                    layers="TOPO-OSM-WMS", format="image/png"), sites, zones_humides]
 # Le layout
 app.layout = html.Div([
-    dl.Map(children = df,
+    html.Div([dl.Map(children = df,
         center=[44.3, 7], zoom=9,
-        style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),
+        style={'width': '100%', 'height': '50vh', 'margin': "auto"}, id="map"),
+    html.Div([dcc.RadioItems(id="selection_zone_humide",options=[{"label": nom, "value": nom}for nom in zh['nom_site'].unique()])], style={'maxHeight':'50vh', 'width':'400px', 'overflowY': 'auto', 'paddingLeft': '1vh'}, id="liste")], style={'display': 'flex'}),
 ])
+
+@app.callback([Output("map", "children"), Output("map", "center")], Input("selection_zone_humide", 'value'))
+def test(nom_site):
+  if nom_site:
+    toutes_zones = zh[zh['nom_site']==nom_site]['geojson']
+    centre = json.loads(points[points['nom_site']==nom_site]['centroid'].all())['coordinates'][::-1]
+    return [dl.WMSTileLayer(url="http://ows.mundialis.de/services/service?",
+                    layers="TOPO-OSM-WMS", format="image/png"), dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry":json.loads(zone)}for zone in toutes_zones]})], centre
+  else:
+    return df, [44.3, 7]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
