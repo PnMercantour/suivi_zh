713ad58932e92a776ff25405025b7d9302926032
Arnaudboy <r.boyer895@laposte.net>
2021-06-29 11:17:22 +0200

    checklist met à jour la mini map et n'accepte qu'une seule zone à la fois. Les polygones de la grande map changent de couleur lorsque l'on passe la souris dessus. Tentative de récupération des infos d'une zone humide dans une troisieme call back pas términée

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash.dependencies import Input, Output, State, MATCH, ALL
 import pandas as pd
+from dash_extensions.javascript import arrow_function
 import json
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -19,7 +20,7 @@ color = {'bon': 'green', 'moyen': 'yellow', 'mauvais': 'red'}
 # GeoJSON pour les points
 sites = dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry" : json.loads(point)} for point in points['centroid']]})
 # GeoJSON pour les zones humides
-zones_humides = dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry":json.loads(site)}for site in zh['geojson']]})
+zones_humides = dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry":json.loads(site), "properties":{"site": "ok"}}for site in zh['geojson']]}, hoverStyle=arrow_function(dict(weight=5, color='#666', dashArray='')), id="zones_humides")
 
 # Création de la dataframe, un tableau passé en paramètre de dl.Map dans le layout
 # Le fond de carte
@@ -30,18 +31,35 @@ app.layout = html.Div([
     html.Div([dl.Map(children = df,
         center=[44.3, 7], zoom=9,
         style={'width': '100%', 'height': '50vh', 'margin': "auto"}, id="map"),
-    html.Div([dcc.RadioItems(id="selection_zone_humide",options=[{"label": nom, "value": nom}for nom in zh['nom_site'].unique()])], style={'maxHeight':'50vh', 'width':'400px', 'overflowY': 'auto', 'paddingLeft': '1vh'}, id="liste")], style={'display': 'flex'}),
+    html.Div([dcc.Checklist(id="selection_zone_humide",options=[{"label": nom, "value": nom}for nom in zh['nom_site'].unique()])], style={'maxHeight':'50vh', 'width':'400px', 'overflowY': 'auto', 'paddingLeft': '1vh'}, id="liste")], style={'display': 'flex'}),
+    dl.Map(style={'width': '30%', 'height': '50vh', 'margin': "auto"},zoom=15,id="mini_map"),
+    html.Div(id="test")
 ])
 
-@app.callback([Output("map", "children"), Output("map", "center")], Input("selection_zone_humide", 'value'))
-def test(nom_site):
+@app.callback(Output('selection_zone_humide', 'value'), Input('selection_zone_humide', 'value'))
+def maj_checlist(v):
+  if v:
+    if len(v) > 1:
+      return v[1:]
+    else:
+      return v
+  else:
+    return []
+
+@app.callback([Output("mini_map", "children"), Output("mini_map", "center")], Input("selection_zone_humide", 'value'))
+def zh_mini_map(nom_site):
   if nom_site:
+    nom_site = nom_site[0]
     toutes_zones = zh[zh['nom_site']==nom_site]['geojson']
     centre = json.loads(points[points['nom_site']==nom_site]['centroid'].all())['coordinates'][::-1]
     return [dl.WMSTileLayer(url="http://ows.mundialis.de/services/service?",
                     layers="TOPO-OSM-WMS", format="image/png"), dl.GeoJSON(data={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry":json.loads(zone)}for zone in toutes_zones]})], centre
   else:
     return df, [44.3, 7]
+
+@app.callback(Output("test", "children"), Input("zones_humides", 'click_feature'))
+def test(val):
+  print(val['properties']['site'])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
