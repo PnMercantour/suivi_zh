a44ad6575f0b83c7c12d88c1e5de39027fdb365d
Vincent Prunet <vincent.prunet@gmail.com>
2021-07-29 16:00:26 +0200

    vallee.py example, dropdown & subpanels

--- vallees.py
+++ vallees.py
@@ -8,6 +8,7 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash.dependencies import Input, Output, State
 from dash_extensions.javascript import assign, arrow_function
+from pathlib import Path
 from dotenv import load_dotenv
 import json
 
@@ -78,93 +79,120 @@ siteLayer = dl.GeoJSON(
 }
 """)))
 
+
+def featurePropertiesFromJson(kind):
+    with open(Path(Path(__file__).parent, 'assets', f'{kind}.json'), 'r') as file:
+        json_data = json.load(file)
+        return [feature['properties'] for feature in json_data['features']]
+
+
+# keep all site options in memory
+site__feature_properties = featurePropertiesFromJson('sites')
+
+vallee_dropdown = dcc.Dropdown(
+    id="valleeDropdown", placeholder='Vallée sélectionnée?', options=[{'label': property['nom'], 'value': property['id']} for property in featurePropertiesFromJson('vallees')])
+
+site_dropdown = dcc.Dropdown(
+    id='siteDropdown', placeholder='Site sélectionné?', options=[{'label': property['nom_site'], 'value': property['id']} for property in site__feature_properties])
+
+overall_info = html.Div(id='overallInfo', hidden=False, children="""
+Cette section s'affiche lorsque aucune région n'est sélectionnée.
+Elle affiche des indicateurs pour l'ensemble du Parc national du Mercantour.
+""")
+vallee_info = html.Div(id='valleeInfo', hidden=True, children="""
+Cette section s'affiche lorsque une vallée est sélectionné, mais pas de site.
+Elle affiche des indicateurs pour l'ensemble de la vallée.
+""")
+site_info = html.Div(id='siteInfo', hidden=True, children="""
+Cette section s'affiche lorsqu'un site est sélectionné.
+Elle affiche le détail des zones humides et des indicateurs pour ce site.""")
 # Le layout
 app.layout = html.Div([
+    html.Div(children=[vallee_dropdown, site_dropdown]),
     dl.Map(id='map', children=[baseLayer, valleeLayer, siteLayer],
            center=[44.3, 7], zoom=9,
            style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),
-    html.Div(dcc.Input(
-        id="valleeInput",
-        type='number',
-        value=None)),
-    html.Div(id="valleeInfo"),
-    html.Div(dcc.Input(
-        id="siteInput",
-        type='number',
-        value=None)),
-    html.Div(id="siteInfo")
+    html.Div(children=[overall_info, vallee_info, site_info])
 ])
 
 
-@ app.callback(Output("siteInfo", "children"), Input("siteLayer", "click_feature"))
-def updateSiteInfo(feature):
-    if feature:
-        return f"Site sélectionné: {feature['properties']['id']}"
-    else:
-        return "Pas de sélection site"
+@app.callback(Output("overallInfo", "hidden"), Output("valleeInfo", "hidden"), Output("siteInfo", "hidden"), Input("siteLayer", "hideout"))
+def toggleInfo(hideout):
+    if hideout['id']:
+        return [True, True, False]
+    if hideout['id_vallee']:
+        return [True, False, True]
+    return [False, True, True]
 
 
-@ app.callback(Output("valleeInfo", "children"), Input("valleeLayer", "click_feature"))
-def updateValleeInfo(feature):
-    if feature:
-        return f"Vallée sélectionnée: {feature['properties']['id']}"
-    else:
-        return "Pas de sélection vallée"
+@ app.callback(Output("siteDropdown", "options"), Input("valleeDropdown", "value"))
+def updateSiteDropdown(id_vallee):
+    return [{'label': property['nom_site'], 'value': property['id']} for property in site__feature_properties if id_vallee == None or property['id_vallee'] == id_vallee]
 
 
 app.clientside_callback(
     """function(
-  site_feature,
-  site_input,
-  vallee_feature,
-  vallee_input,
-  click_lat_lng,
+  site_feature, vallee_feature, click_lat_lng,
+  site_dropdown,
+  vallee_dropdown,
   hideout
 ) {
-  console.log(dash_clientside.callback_context);
+  //console.log(dash_clientside.callback_context);
   const triggers = dash_clientside.callback_context.triggered;
+
+  let new_site_feature = null, new_vallee_feature = null;
+
   if (triggers.some((o) => o.prop_id == "siteLayer.click_feature")){
     // update "site" and "vallee"
-    return [
-      {id: site_feature.properties.id, id_vallee: site_feature.properties.id_vallee},
-      site_feature,
-      site_feature.properties.id,
-      cachedData.valleeTable[site_feature.properties.id_vallee],
-      site_feature.properties.id_vallee
-    ];
-  }
+    new_site_feature = site_feature;
+  } else
   if (triggers.some((o) => o.prop_id == "valleeLayer.click_feature")) {
     // update "vallee" and reset "site"
-    return [{id: undefined, id_vallee: vallee_feature.properties.id}, undefined, undefined, cachedData.valleeTable[vallee_feature.properties.id], vallee_feature.properties.id];
-  }
+    new_site_feature = null;
+    new_vallee_feature = vallee_feature;
+  } else
   if (triggers.some((o) => o.prop_id == "map.click_lat_lng")){
-    return [{id: undefined, id_vallee: undefined}, undefined, undefined, undefined, undefined];
+    new_site_feature = null;
+    new_vallee_feature = null;
+  } else
+  if (triggers.some((o) => o.prop_id == "siteDropdown.value")){
+    new_site_feature = site_dropdown? cachedData.siteTable[site_dropdown] : null;
+  } else
+  if (triggers.some((o) => o.prop_id == "valleeDropdown.value")){
+    new_site_feature = null;
+    new_vallee_feature = vallee_dropdown? cachedData.valleeTable[vallee_dropdown]: null;
   }
-  if (triggers.some((o) => o.prop_id == "siteInput.value")){
-    let new_site_feature = site_input? cachedData.siteTable[site_input] : undefined;
-    let new_id_vallee = new_site_feature? new_site_feature.properties.id_vallee: undefined;
-    let vallee_feature = new_id_vallee? cachedData.valleeTable[new_id_vallee]: undefined;
-    return [{id: site_input, id_vallee: new_id_vallee}, new_site_feature, site_input, vallee_feature, new_id_vallee];
-    }
-  if (triggers.some((o) => o.prop_id == "valleeInput.value")){
-    let vallee_feature = vallee_input? cachedData.valleeTable[vallee_input]: undefined;
-    return [{id: undefined, id_vallee: vallee_input}, undefined, undefined, vallee_feature, vallee_input];
+
+  let id = null, id_vallee = null;
+  if (new_site_feature != null) {
+      id = new_site_feature.properties.id;
+      id_vallee = new_site_feature.properties.id_vallee;
+      new_vallee_feature = cachedData.valleeTable[id_vallee];
+  } else if (new_vallee_feature != null) {
+      id_vallee = new_vallee_feature.properties.id;
+  } else {
   }
-  return[{id: undefined, id_vallee: undefined}, undefined, undefined, undefined, undefined];
+  let result = [
+    {...hideout, id, id_vallee}, new_site_feature, //siteLayer
+    new_vallee_feature, //valleeLayer
+    id, //siteDropdown
+    id_vallee, //valleeDropdown
+    ];
+  //console.log('result = ', result);
+  return result;
 }
 """,
-    Output("siteLayer", "hideout"),
-    Output("siteLayer", "click_feature"),
-    Output("siteInput", "value"),
+    Output("siteLayer", "hideout"), Output("siteLayer", "click_feature"),
     Output("valleeLayer", 'click_feature'),
-    Output("valleeInput", "value"),
-    Input("siteLayer", "click_feature"),
-    Input("siteInput", "value"),
-    Input("valleeLayer", "click_feature"),
-    Input("valleeInput", "value"),
-    Input("map", "click_lat_lng"),
-    State("siteLayer", "hideout"))
+    Output("siteDropdown", "value"),
+    Output("valleeDropdown", "value"),
 
+    Input("siteLayer", "click_feature"),
+    Input("valleeLayer", "click_feature"),
+    Input("map", "click_lat_lng"),
+    Input("siteDropdown", "value"),
+    Input("valleeDropdown", "value"),
+    State("siteLayer", "hideout"))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
