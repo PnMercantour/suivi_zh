ad11abb041b5e47b39b4ebc70ea7520b57d6f6c5
Arnaudboy <r.boyer895@laposte.net>
2021-08-03 20:26:37 +0200

    partie haute interdépendance, partie basse detail_site -> zoneLayer, zoneTable et pieChart branchés

--- app.py
+++ app.py
@@ -15,20 +15,23 @@ import json
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 load_dotenv('.env/.env')
 
+#=====CREATION DE L'APP=====#
 app = dash.Dash(__name__)
 
-df = pandas.read_csv(
-    Path(Path(__file__).parent, 'assets', 'habitat.csv'))
-
+#=====PARTIE DONNEES JSON ET CSV POUR L'APP=====#
 def featurePropertiesFromJson(kind):
     with open(Path(Path(__file__).parent, 'assets', f'{kind}.json'), 'r') as file:
         json_data = json.load(file)
         return [feature['properties'] for feature in json_data['features']]
 
 # keep all site options in memory
-site__feature_properties = featurePropertiesFromJson('sites')
+site_feature_properties = featurePropertiesFromJson('sites')
+# Pie Chart csv
+df = pandas.read_csv(
+    Path(Path(__file__).parent, 'assets', 'habitat.csv'))
 
-#création d'un dictionaire pour les couleurs des polygones
+#=====CREATION DES FONCTION POUR LES LAYERS=====#
+# création d'un dictionaire pour les couleurs des polygones
 js_style = assign("""
 function(feature, context) {
     if (context.props.hideout && feature.properties.id == context.props.hideout.selected_zone) {return {color:"#000000"}}
@@ -36,21 +39,8 @@ function(feature, context) {
         let t = {bon: "#1E90FF", moyen: "#FF7F50", mauvais: "#FF4500"};
         return {color: t[feature.properties.etat_zh]}}
 }""")
-
-fonction_couleur_carte = assign("""
-(feature, layer) => {
-    if(!feature.properties){
-        return
-    }
-    console.log(feature)
-    cachedData.zhTable[feature.properties.id] = {surface : feature.properties.surface, etat: feature.properties.etat_zh}
-    if(feature.properties.etat_zh){
-        layer.bindTooltip(feature.properties.etat_zh)
-    }
-}
-""")
-
-sitePointToLayer = assign("""function(feature, latlng, context){
+# Give a color to each feature on siteLayer
+site_point_to_layer = assign("""function(feature, latlng, context){
     if (context.props.hideout) {
         if (feature.properties.id == context.props.hideout.selected_site) {
             circleOptions= {color: "red", fillColor: "red", fillOpacity: 0.8};
@@ -64,25 +54,35 @@ sitePointToLayer = assign("""function(feature, latlng, context){
     }
     return L.circleMarker(latlng, circleOptions)  // send a simple circle marker.
 }""")
-
-# GeoJSON pour les sites
-siteLayer = dl.GeoJSON(id="siteLayer", url=app.get_asset_url('sites.json'), hideout={'selected_site': None, 'selected_vallee': None}, options=dict(pointToLayer=sitePointToLayer, onEachFeature=assign("""
-    (feature, layer) => {
-        if(!feature.properties){
-            return
-        }
-        cachedData.siteTable[feature.properties.id] = feature
-        if(feature.properties.nom_site){
-            layer.bindTooltip(feature.properties.nom_site)
-        }
+# Give a color to each feature on zhLayer
+couleur_zh = assign("""
+(feature, layer) => {
+    if(!feature.properties){
+        return
+    }
+    cachedData.zhTable[feature.properties.id] = {surface : feature.properties.surface, etat: feature.properties.etat_zh}
+    if(feature.properties.etat_zh){
+        layer.bindTooltip(feature.properties.etat_zh)
+    }
 }
-""")))
+""")
 
-zhLayer = dl.GeoJSON(id='zhLayer', hideout={'selected_zone': None, 'selected_site': None, 'nom_site': None},
-    options=dict( 
-    onEachFeature=fonction_couleur_carte, style=js_style), 
-    zoomToBounds=True)
-
+#=====CREATION DES LAYERS POUR L'APP=====#
+# Le fond de carte
+baseLayer = dl.TileLayer(url="https://wxs.ign.fr/" + os.getenv('IGN_KEY') + "/wmts?" +
+                        "&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0" +
+                        "&STYLE=normal" +
+                        "&TILEMATRIXSET=PM" +
+                        "&FORMAT=image/jpeg" +
+                        "&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS" +
+                        "&TILEMATRIX={z}" +
+                        "&TILEROW={y}" +
+                        "&TILECOL={x}",
+                        minZoom=0,
+                        maxZoom=18,
+                        tileSize=256,
+                        attribution="IGN-F/Geoportail")
+# GeoJSON pour les vallées
 valleeLayer = dl.GeoJSON(
     url=app.get_asset_url('vallees.json'),
     id="valleeLayer",
@@ -97,24 +97,29 @@ valleeLayer = dl.GeoJSON(
     }
 }
 """)))
+# GeoJSON pour les sites
+site_layer = dl.GeoJSON(id="siteLayer", url=app.get_asset_url('sites.json'), hideout={'selected_site': None, 'selected_vallee': None}, options=dict(pointToLayer=site_point_to_layer, onEachFeature=assign("""
+    (feature, layer) => {
+        if(!feature.properties){
+            return
+        }
+        cachedData.siteTable[feature.properties.id] = feature
+        if(feature.properties.nom_site){
+            layer.bindTooltip(feature.properties.nom_site)
+        }
+}
+""")))
+# GeoJSON pour les zones humides
+zh_layer = dl.GeoJSON(id='zhLayer', hideout={'selected_zone': None, 'selected_site': None, 'nom_site': None},
+    options=dict( 
+    onEachFeature=couleur_zh, style=js_style), 
+    zoomToBounds=True)
 
-zhTable = dash_table.DataTable(
-            id='zhTable',
-            columns=[],
-            data=[],
-            sort_action='native',
-            filter_action='native',
-            style_data_conditional=[
-            {'if': {'row_index': 'odd'},
-            'backgroundColor': 'rgb(248, 248, 248)'
-            }
-        ], page_size=10,  merge_duplicate_headers=True,
-)
-
+#=====CREATION DES DATATABLES POUR L'APP=====#
 # siteTable = dash_table.DataTable(
 #         id='siteTable',
 #         columns=[{"name": "nom_site", "id": "nom_site"}],
-#         data=[{'nom_site':index}['nom_site'] for index in site__feature_properties],
+#         data=[{'nom_site':index}['nom_site'] for index in site_feature_properties],
 #         sort_action='native',
 #         filter_action='native',
 #         style_data_conditional=[
@@ -124,39 +129,44 @@ zhTable = dash_table.DataTable(
 #         ]
 # )
 
-# Création de la dataframe, un tableau passé en paramètre de dl.Map dans le layout
-# Le fond de carte
-baseLayer = dl.TileLayer(url="https://wxs.ign.fr/" + os.getenv('IGN_KEY') + "/wmts?" +
-                         "&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0" +
-                         "&STYLE=normal" +
-                         "&TILEMATRIXSET=PM" +
-                         "&FORMAT=image/jpeg" +
-                         "&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS" +
-                         "&TILEMATRIX={z}" +
-                         "&TILEROW={y}" +
-                         "&TILECOL={x}",
-                         minZoom=0,
-                         maxZoom=18,
-                         tileSize=256,
-                         attribution="IGN-F/Geoportail")
+zhTable = dash_table.DataTable(
+            id='zhTable',
+            columns=[],
+            data=[],
+            style_data_conditional=[
+            {'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(248, 248, 248)'
+            }
+        ], page_size=10,  merge_duplicate_headers=True,
+)
 
-# Le layout
+#=====CREATION DU LAYOUT=====#
 app.layout = html.Div([
     html.Div(id="dropdownDiv", children=[
-        dcc.Dropdown(id="valleeDropdown", options = [{'label': property['nom'], 'value': property['id']} for property in featurePropertiesFromJson('vallees')]), 
-        dcc.Dropdown(id="siteDropdown")
+        dcc.Dropdown(id="valleeDropdown", options = [{'label': property['nom'], 'value': property['id']} for property in featurePropertiesFromJson('vallees')], placeholder = "Sélection d'une vallée"), 
+        dcc.Dropdown(id="siteDropdown", placeholder = "Sélection d'un site")
     ]),
     html.Div(
-        dl.Map(id="parc", children = [baseLayer, valleeLayer, siteLayer],
+        dl.Map(id="parc", children = [baseLayer, valleeLayer, site_layer],
         center=[44.3, 7], zoom=9),style={'display':'flex', 'paddingBottom':'5vh'}),
-    html.Div("Lorem ipsum", id="detail_parc", style={'display':'none'}), html.Div(id="detail_vallee", style={'display':'none'}),
-    html.Div(id="detail_site",children=[
-        dl.Map(id="site_unique", children=[baseLayer, zhLayer], center=[44.3, 7]),
+    html.Div("Lorem ipsum", id="detailParc", hidden=True), html.Div(id="detailVallee", hidden=True),
+    html.Div(id="detailSite",children=[
+        dl.Map(id="site_unique", children=[baseLayer, zh_layer], center=[44.3, 7]),
         zhTable,
         dcc.Graph(id="pie-chart", figure=px.pie(df[df["id_zh"] == 374],
-              values='proportion', names='code'), style={"backgroundColor": "yellow"})
-    ], style={'visibility':'hidden'}),
+              values='proportion', names='code'))
+    ]),
 ])
+#=====LES CALLBACKS=====#
+
+#=====LES CALLBACKS PARTIE HAUTE=====#
+@app.callback(Output("siteDropdown", "options"), Input("valleeDropdown", "value"))
+def dropdownsOptions(valleeValue):
+    if valleeValue != None:
+        siteOptions = [{'label': index['nom_site'], 'value': index['id']} for index in filter(lambda item : item['id_vallee']==valleeValue, site_feature_properties)]
+    else:
+        siteOptions = [{'label': property['nom_site'], 'value': property['id']} for property in site_feature_properties]
+    return siteOptions
 
 app.clientside_callback(
     """function(dropdown_value, valleeDropdown_value, site_feature, vallee_feature, click_lat_lng) {
@@ -167,7 +177,7 @@ app.clientside_callback(
         if(triggers.some((o) => o.prop_id === "siteLayer.click_feature" )){
             return_hideout = {selected_site: site_feature.properties.id, selected_vallee: site_feature.properties.id_vallee, nom_site: site_feature.properties.nom_site}
         } else
-        if(triggers.some((o) => o.prop_id === "siteDropdown.value")) {
+        if(triggers.some((o) => o.prop_id === "siteDropdown.value")) {//filtrer par id
             return_hideout = {selected_site: dropdown_value, selected_vallee: dropdown_value ? cachedData.siteTable.filter((elem) => elem.properties.id === dropdown_value)[0].properties.id_vallee : valleeDropdown_value}
         }
         // following statement updates "vallee" reset "site" => both valleeLayer and valleeDropdown could update selected_vallee
@@ -201,26 +211,34 @@ app.clientside_callback(
     Input("parc", "click_lat_lng"), 
 )
 
-@ app.callback(Output("siteDropdown", "options"), Input("valleeDropdown", "value"))
-def dropdownsOptions(valleeValue):
-    if valleeValue != 0 and valleeValue != None:
-        siteOptions = [{'label': index['nom_site'], 'value': index['id']} for index in filter(lambda item : item['id_vallee']==valleeValue, site__feature_properties)]
-    else:
-        siteOptions = [{'label': property['nom_site'], 'value': property['id']} for property in site__feature_properties]
-    return siteOptions
-    
-@app.callback([Output("detail_parc", "style"), Output("detail_vallee", "style"), Output("detail_site", "style")], Input("siteLayer", "hideout"), State("detail_parc", "style"), State("detail_vallee", "style"), State("detail_site", "style"))
-def detail(hideout, detail_parcStyle, detail_valleeStyle, detail_siteStyle):
-    detail_parcStyle.update({'display':'none'})
-    detail_valleeStyle.update({'display':'none'})
-    detail_siteStyle.update({'visibility':'hidden'})
+#=====CALLBACK DE GESTION DES PARTIES DETAILS=====#
+@app.callback([Output("detailParc", "hidden"), Output("detailVallee", "hidden"), Output("detailSite", "hidden")], Input("siteLayer", "hideout"))
+def detail(hideout):
     if all(propertie==None for propertie in hideout.values()) :
-        detail_parcStyle.update({'display':'inline'})
+        return [False, True, True]
     elif hideout['selected_vallee'] is not None and hideout['selected_site'] is None:
-        detail_valleeStyle.update({'display':'inline'})
-    else  :
-        detail_siteStyle.update({'visibility':'visible'})
-    return detail_parcStyle, detail_valleeStyle, detail_siteStyle
+        return [True, False, True]
+    else :
+        return [True, True, False]
+
+#=====LES CALLBACKS DE detailSite=====#
+@app.callback([Output("zhLayer", "url"), Output("zhTable", "data")], Input("siteLayer", "hideout"))
+def zhLayer_url(hideout):
+    if hideout['selected_site']:
+        data = map(lambda x : {'surface': x['surface'], 'etat': x['etat_zh']}, featurePropertiesFromJson("sites/"+str(hideout['selected_site'])))
+        url = "/assets/sites/"+str(hideout['selected_site'])+".json"
+        return url, list(data)
+    else:
+        raise PreventUpdate
+
+@app.callback(
+    Output("pie-chart", "figure"),
+    Input("zhLayer", "hideout"))
+def generate_chart(hideout):
+    if hideout:
+        id_zh = hideout['selected_zone']
+        fig = px.pie(df[df["id_zh"] == id_zh], values='proportion', names='code')
+        return fig
 
 app.clientside_callback(
         """function(zhfeature, hideout){
@@ -239,24 +257,6 @@ app.clientside_callback(
         Input("zhLayer", "click_feature"),
         Input("siteLayer", "hideout"),
 )
-
-@app.callback([Output("zhLayer", "url"), Output("zhTable", "data")], Input("siteLayer", "hideout"))
-def zhLayer_url(hideout):
-    if hideout['selected_site']:
-        data = map(lambda x : {'surface': x['surface'], 'etat': x['etat_zh']}, featurePropertiesFromJson("sites/"+str(hideout['selected_site'])))
-        url = "/assets/sites/"+str(hideout['selected_site'])+".json"
-        return url, list(data)
-    else:
-        raise PreventUpdate
-        
-@app.callback(
-    Output("pie-chart", "figure"),
-    Input("zhLayer", "hideout"))
-def generate_chart(hideout):
-    if hideout:
-        id_zh = hideout['selected_zone']
-        fig = px.pie(df[df["id_zh"] == id_zh], values='proportion', names='code')
-        return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
