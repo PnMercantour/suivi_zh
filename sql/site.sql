38a73bc4645ad0e09c391cb82187b4a23dd2a106
Vincent Prunet <vincent.prunet@gmail.com>
2022-05-06 14:26:01 +0200

    site.sql

--- sql/site.sql
+++ sql/site.sql
@@ -2,29 +2,42 @@
 -- properties:
 --      id_site (entier)
 --      nom_site (chaîne UTF8)
+--		id_vallee
+--		n_zh (nombre de zh du site ou null)
+--		s_zh (surface totale des zh du site ou null)
+--		n_defens(nombre de défens du site ou null)
+--		s_defens (surface totale des défens du site ou null)
+--		ratio (indicateur de pourcentage de surface de bonne qualité)
 -- geometry: point (le centroide des zh du site)
 -- bbox : la boîte englobante des zh du site
 -- https://datatracker.ietf.org/doc/html/rfc7946
-
-with envelope as (
-select nom_site, box2d(st_transform( st_union(geom), 4326)) b from eau_zh.zh
-group by nom_site
+WITH site_geo AS (
+  SELECT
+    id_site,
+    nom_site,
+    vallee.id id_vallee,
+    st_transform (v_site.geom, 4326) geom,
+    box2d (st_transform (bbox, 4326)) b,
+    n_zh,
+    s_zh,
+    n_defens,
+    s_defens
+  FROM
+    eau_zh.v_site
+    LEFT JOIN limregl.cr_pnm_vallees_topo vallee ON (st_within (v_site.geom, vallee.geom))
 ),
-	features as (
-select json_build_object(
-'type', 'Feature',
-'properties', json_build_object(
-	'id_site', site.id,
-	'nom_site', nom_site, 
-	'id_vallee', vallee.id
-	),
-'geometry', st_asgeojson(st_transform(site.geom, 4326))::json,
-'bbox', json_build_array(st_xmin(b), st_ymin(b), st_xmax(b), st_ymax(b)) 
-) feature 
-from eau_zh.site left join envelope using(nom_site) join limregl.cr_pnm_vallees_topo vallee on(st_within (site.geom, vallee.geom))
-)
-select json_build_object(
-	'type', 'FeatureCollection',
-	'features', json_agg(feature)
-	)::text geojson
-from features;
+features AS (
+  SELECT
+    json_build_object('type', 'Feature', 'properties',
+      json_build_object('id_site', id_site, 'nom_site', nom_site, 'id_vallee',
+      id_vallee, 'n_zh', n_zh, 's_zh', s_zh, 'n_defens', n_defens,
+      's_defens', s_defens, 'ratio', ratio), 'geometry', st_asgeojson
+      (site_geo.geom)::json, 'bbox', json_build_array(st_xmin (b), st_ymin (b), st_xmax
+      (b), st_ymax (b))) feature
+  FROM
+    site_geo
+    LEFT JOIN eau_zh.v_site_etat USING (nom_site))
+SELECT
+  json_build_object('type', 'FeatureCollection', 'features', json_agg(feature))::text geojson
+FROM
+  features;
