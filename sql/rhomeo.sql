726c0d6a8324199da608a18ca674d6db4665a846
Vincent Prunet <vincent.prunet@gmail.com>
2022-07-05 16:57:54 +0200

    rhomeo

--- sql/rhomeo.sql
+++ sql/rhomeo.sql
@@ -1,24 +1,33 @@
 -- construit un objet geojson avec les relevés rhomeo
-
 WITH releves AS (
   SELECT
-    geom, count(*) releves, 'CBNA' organisme from eau."zh_rhomeo_releves_placettes_ZH_PNM_CBNA_2020"
-    group by geom, organisme
-union
-    SELECT
-    geom, count(*) releves, 'CBNMed' organisme from eau."zh_rhomeo_releves_placettes_ZH_PNM_CBNMED_2020"
-    group by geom, organisme
-  ),
-features as (
-  select json_build_object(
-    'type', 'Feature', 
-    'properties', json_build_object(
-        'organisme', organisme,
-        'releves', releves
-    ),
-    'geometry', st_asgeojson
-      (st_transform(releves.geom, 4326), 6)::json
-) feature from releves)
+    geom,
+    count(*) releves,
+    'CBNA' organisme
+  FROM
+    eau_zh."zh_rhomeo_releves_placettes_ZH_PNM_CBNA_2020"
+  GROUP BY
+    geom,
+    organisme
+  UNION
+  SELECT
+    geom,
+    count(*) releves,
+    'CBNMed' organisme
+  FROM
+    eau_zh."zh_rhomeo_releves_placettes_ZH_PNM_CBNMED_2020"
+  GROUP BY
+    geom,
+    organisme
+),
+features AS (
+  SELECT
+    json_build_object('type', 'Feature', 'properties',
+      json_build_object('organisme', organisme, 'releves', releves), 'geometry',
+      st_asgeojson (st_transform (releves.geom, 4326), 6)::json) feature
+  FROM
+    releves
+)
 SELECT
   json_build_object('type', 'FeatureCollection', 'features', json_agg(feature))::text geojson
 FROM
