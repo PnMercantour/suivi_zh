d5fb6e392f971216044db231b0b0506ab146014a
Vincent Prunet <vincent.prunet@gmail.com>
2022-07-07 17:44:15 +0200

    sort notices

--- sql/notice.sql
+++ sql/notice.sql
@@ -1,31 +1,19 @@
 WITH features AS (
-  SELECT json_build_object(
-      'type',
-      'Feature',
-      'properties',
-      json_build_object(
-        'id',
-        id,
-        'nom',
-        nom,
-        'id_site',
-        id_site,
-        'description',
-        description,
-        'auteur',
-        auteur,
-        'date',
-        date,
-        'langue',
-        langue
-      )
-    ) feature
-  FROM eau_zh.notice
-)
-SELECT json_build_object(
-    'type',
-    'FeatureCollection',
-    'features',
-    json_agg(feature)
-  )::text geojson
-FROM features;
+  SELECT
+    json_build_object('type', 'Feature', 'properties',
+      json_build_object('id', notice.id, 'nom', notice.nom, 'id_site',
+      id_site, 'description', description, 'auteur', auteur, 'date', date,
+      'langue', langue)) feature,
+    site.nom_site
+  FROM
+    eau_zh.notice
+    LEFT JOIN eau_zh.site ON (id_site = site.id))
+SELECT
+  json_build_object('type', 'FeatureCollection', 'features', json_agg(feature))::text geojson
+FROM (
+  SELECT
+    feature
+  FROM
+    features
+  ORDER BY
+    nom_site) f;
